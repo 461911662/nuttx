ea112f3aeb57b3c0a096ccdac6e637a2031b013e
461911662 <461911662@qq.com>
2026-03-20 14:00:00 +0800

    boards/esp32s3-boss1-cx: Add board_spiflash_init declaration
    Add function declaration in esp32s3-boss1-cx.h to fix
    implicit declaration warning.

--- boards/xtensa/esp32s3/esp32s3-boss1-cx/src/esp32s3-boss1-cx.h
+++ boards/xtensa/esp32s3/esp32s3-boss1-cx/src/esp32s3-boss1-cx.h
@@ -69,5 +69,17 @@ int esp32s3_bringup(void);
 void esp32s3_led_initialize(void);
 #endif
 
+/****************************************************************************
+ * Name: board_spiflash_init
+ *
+ * Description:
+ *   Initialize the SPIFLASH and register the MTD device.
+ *
+ ****************************************************************************/
+
+#ifdef CONFIG_ESP32S3_SPIFLASH
+int board_spiflash_init(void);
+#endif
+
 #endif /* __ASSEMBLY__ */
 #endif /* __BOARDS_XTENSA_ESP32S3_ESP32S3_BOSS1_CX_SRC_ESP32S3_BOSS1_CX_H */
